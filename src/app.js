f19f15a08979c917becd76db4730271326e0dbf0
Natan Shaltiel <snatan@gmail.com>
2023-01-05 09:52:22 +0200

    Task1 only return the contract if the profile matches the ID

--- src/app.js
+++ src/app.js
@@ -14,7 +14,12 @@ app.set('models', sequelize.models)
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    let contract;
+    if (id == req.profile.id) { 
+        //using == as we want to compare string to an int (and I didn't see the need to parse it)
+        contract = await Contract.findOne({where: {id}})
+    }
+    
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
