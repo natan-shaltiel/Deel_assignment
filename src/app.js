8ee1739b21abe201b3d66b1a78ebce196abba1c5
Natan Shaltiel <snatan@gmail.com>
2023-01-05 10:46:56 +0200

    fixed task1 after I realized that belongs means client or contractor

--- src/app.js
+++ src/app.js
@@ -15,9 +15,11 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
     let contract;
-    if (id == req.profile.id) { 
-        //using == as we want to compare string to an int (and I didn't see the need to parse it)
-        contract = await Contract.findOne({where: {id}})
+    const profileId = req.profile.id
+    contract = await Contract.findOne({where: {id}})
+    if(!contract || (contract.ContractorId != profileId && contract.ClientId != profileId)) return res.status(404).end()
+    res.json(contract)
+})
     }
     
     if(!contract) return res.status(404).end()
